9b5aeef7c2990842c9fead86b9098956b382de43
tdincer <tolgadincer@gmail.com>
2022-06-23 14:34:48 -0500

    modify do_pose_estimation to write config.yaml in dlc_project_path

--- element_deeplabcut/readers/dlc_reader.py
+++ element_deeplabcut/readers/dlc_reader.py
@@ -178,22 +178,24 @@ def do_pose_estimation(
     # ---- Build and save DLC configuration (yaml) file ----
     dlc_config = dlc_model["config_template"]
     dlc_project_path = Path(project_path)
-    assert (project_path / "config.yaml").exists(), (
-        "DLC needs the config.yaml in the " + f"project path: {project_path}"
-    )
     dlc_config["project_path"] = dlc_project_path.as_posix()
 
     # ---- Write DLC and basefolder yaml (config) files ----
     # Write dlc config file to base (data) folder
     # This is important for parsing the DLC in datajoint imaging
+    # This is required to load the results
     output_dir.mkdir(exist_ok=True)
-    dlc_cfg_filepath = output_dir / "dlc_config_file.yaml"
+    with open(output_dir / "dlc_config_file.yaml", "w") as f:
+        yaml.dump(dlc_config, f)
+
+    # This is required by DLC to run the analyze_videos
+    dlc_cfg_filepath = dlc_project_path / "dlc_config_file.yaml"
     with open(dlc_cfg_filepath, "w") as f:
         yaml.dump(dlc_config, f)
 
     # ---- Trigger DLC prediction job ----
     analyze_videos(
-        config=(project_path / "config.yaml"),
+        config=dlc_cfg_filepath.as_posix(),
         videos=video_filepaths,
         shuffle=dlc_model["shuffle"],
         trainingsetindex=dlc_model["trainingsetindex"],
