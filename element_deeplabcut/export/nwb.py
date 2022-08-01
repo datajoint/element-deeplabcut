6121e707c103765b60f708d421c1610547e50203
Chris Broz <CBroz@datajoint.com>
2022-08-01 11:32:51 -0500

    Refactor based on DLC2NWB#10

--- element_deeplabcut/export/nwb.py
+++ element_deeplabcut/export/nwb.py
@@ -1,13 +1,18 @@
-import warnings
+"""
+Portions of code adapted from DeepLabCut/DLC2NWB
+MIT License Copyright (c) 2022 Alexander Mathis
+DataJoint export methods for DeepLabCut 2.x
+"""
 import logging
-from pynwb import NWBHDF5IO
-from hdmf.build.warnings import DtypeConversionWarning
+import warnings
 from pathlib import Path
 from collections import abc
+from pynwb import NWBHDF5IO
+from hdmf.build.warnings import DtypeConversionWarning
 from .. import model
 
 try:  # Not all users will want NWB export, so dependency not in requirements.
-    from dlc2nwb.utils import convert_h5_to_nwb
+    from dlc2nwb.utils import convert_h5_to_nwb, write_subject_to_nwb
 except ImportError:
     raise ImportError(
         "The package `dlc2nwb` is missing. Please run `pip install dlc2nwb`."
@@ -20,40 +25,48 @@ def dlc_session_to_nwb(keys, use_element_session=True, session_kwargs=None):
     """Using keys from PoseEstimation table, save DLC's h5 output to NWB.
 
     Calls DLC2NWB to export NWB file using current h5 on disk. If use_element_session,
-    calls NWB export function from element session, passing session_kwargs.
+    calls NWB export function from Elements for lab, animal and session, passing
+    session_kwargs. Saves output based on naming convention in DLC2NWB. If output path
+    already exists, returns output path without making changes to the file.
+    NOTE: does not support multianimal exports
 
     Parameters
     ----------
     Keys - one or more keys from model.PoseEstimation
     use_element_session - Optional. If True, call NWB export from Element Session
-    session_kwargs - Optiona. Additional keyword arguements for Element Session export
+    session_kwargs - Optional. Additional keyword arguements for Element Session export
+
+    Returns output path of saved file
     """
     if not isinstance(keys, abc.Sequence):  # Ensure list for following loop
         keys = [keys]
 
     for key in keys:
+        write_file = True
         subject_id = key["subject"]
         output_dir = model.PoseEstimationTask.infer_output_dir(key)
+        config_file = str(output_dir / "dj_dlc_config.yaml")
         video_name = Path((model.VideoRecording.File & key).fetch1("file_path")).stem
         h5file = str(list(output_dir.glob(f"{video_name}*h5"))[0])
-        output_path = convert_h5_to_nwb(  # Saves it's own NWB
-            str(output_dir / "dj_dlc_config.yaml"), h5file, subject_id
-        )
+        output_path = h5file.replace(".h5", f"_{subject_id}.nwb")  # DLC2NWB convention
 
-        if use_element_session:  # rewrites nwb saved above with session_to_nwb data
-            # TODO: refactor after merge: https://github.com/DeepLabCut/DLC2NWB/pull/10
+        if Path(output_path).exists():
+            logger.warning(f"Skipping {subject_id}. NWB already exists.")
+            write_file = False
+
+        # Use standard DLC2NWB export
+        if write_file and not use_element_session:
+            output_path = convert_h5_to_nwb(config_file, h5file, subject_id)
+
+        # Pass Element Session export items in export
+        if write_file and use_element_session:
             from element_session.export.nwb import session_to_nwb
 
-            with NWBHDF5IO(output_path[0], "r") as io:
-                dlcnwb = io.read()  # read NWB file saved above
             session_nwb = session_to_nwb(key, **session_kwargs)  # call session export
-            behavior_pm = session_nwb.create_processing_module(
-                name="behavior", description="processed behavioral pose estimation"
-            )
-            behavior_pm.add(dlcnwb.processing)
-
+            dlc_nwb = write_subject_to_nwb(session_nwb, h5file, subject_id, config_file)
+            # warnings filter from DLC2NWB
             with warnings.catch_warnings(), NWBHDF5IO(output_path, mode="w") as io:
                 warnings.filterwarnings("ignore", category=DtypeConversionWarning)
-                io.write(session_nwb)
+                io.write(dlc_nwb)
 
-        logger.info(f"Saved NWB:\n\t{output_path}")
+    return output_path
