d4f00359363670f148fed8d3c66caf875c57d0a0
Tolga Dincer <tolgadincer@gmail.com>
2022-11-07 10:17:47 -0600

    reorganize the calculations & change attribute names

--- element_deeplabcut/model_report.py
+++ element_deeplabcut/model_report.py
@@ -33,14 +33,14 @@ class PoseEstimationReport(dj.Computed):
 
     Attributes:
         PoseEstimation (foreign key): Pose estimation key.
-        position_trace (longblob): Plotly figure of predictions as a function time, in json string format.
-        trajectory (longblob): Plotly figure of predictions to show 2d trajectories."""
+        position_space (longblob): Plotly figure of predictions as a function time, in json string format.
+        position_time (longblob): Plotly figure of predictions to show 2d trajectories."""
 
     definition = """
     -> model.PoseEstimation
     ---
-    position_trace: longblob
-    trajectory: longblob
+    position_space: longblob
+    position_time: longblob
     """
 
     def make(self, key):
@@ -48,7 +48,23 @@ class PoseEstimationReport(dj.Computed):
             model.PoseEstimation.BodyPartPosition * model.RecordingInfo & key
         ).fetch("body_part", "x_pos", "y_pos", "fps")
 
-        position_trace_fig = go.Figure(
+        position_space_fig = go.Figure(
+            [
+                go.Scatter(
+                    x=x,
+                    y=y,
+                    name=name,
+                    mode="markers",
+                    marker=dict(size=3, color=color),
+                )
+                for name, x, y, color in zip(
+                    body_part, x_pos, y_pos, DEFAULT_PLOTLY_COLORS
+                )
+            ]
+        )
+        position_space_fig.update_yaxes(autorange="reversed")
+
+        position_time_fig = go.Figure(
             [
                 go.Scatter(
                     x=np.r_[: len(x_pos[0])] / fps_i,
@@ -68,26 +84,10 @@ class PoseEstimationReport(dj.Computed):
             ]
         )
 
-        trajectory_fig = go.Figure(
-            [
-                go.Scatter(
-                    x=x,
-                    y=y,
-                    name=name,
-                    mode="markers",
-                    marker=dict(size=3, color=color),
-                )
-                for name, x, y, color in zip(
-                    body_part, x_pos, y_pos, DEFAULT_PLOTLY_COLORS
-                )
-            ]
-        )
-        trajectory_fig.update_yaxes(autorange="reversed")
-
         self.insert1(
             {
                 **key,
-                "position_trace": position_trace_fig.to_json(),
-                "trajectory": trajectory_fig.to_json(),
+                "position_trace": position_time_fig.to_json(),
+                "trajectory": position_space_fig.to_json(),
             }
         )
