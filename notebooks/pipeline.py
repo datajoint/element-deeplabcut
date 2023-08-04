be51282812e58cb96d2bfcc9e7bb34d46ab39475
Milagros Marin <milagros@datajoint.com>
2023-08-04 12:55:32 -0500

    merge paths and pipeline files, move to notebooks

--- notebooks/pipeline.py
+++ notebooks/pipeline.py
@@ -1,4 +1,5 @@
 import datajoint as dj
+from collections import abc
 from element_lab import lab
 from element_animal import subject
 from element_session import session_with_datetime as session
@@ -7,11 +8,7 @@ from element_deeplabcut import train, model
 from element_animal.subject import Subject
 from element_lab.lab import Source, Lab, Protocol, User, Project
 
-from .paths import get_dlc_root_data_dir, get_dlc_processed_data_dir
-
 __all__ = [
-    "get_dlc_root_data_dir",
-    "get_dlc_processed_data_dir",
     "Subject",
     "Source",
     "Lab",
@@ -25,6 +22,29 @@ if "custom" not in dj.config:
     dj.config["custom"] = {}
 
 db_prefix = dj.config["custom"].get("database.prefix", "")
+
+
+def get_dlc_root_data_dir() -> list:
+    """Returns a list of root directories for Element DeepLabCut"""
+    dlc_root_dirs = dj.config.get("custom", {}).get("dlc_root_data_dir")
+    if not dlc_root_dirs:
+        return None
+    elif not isinstance(dlc_root_dirs, abc.Sequence):
+        return list(dlc_root_dirs)
+    else:
+        return dlc_root_dirs
+
+
+def get_dlc_processed_data_dir() -> str:
+    """Returns an output directory relative to custom 'dlc_output_dir' root"""
+    from pathlib import Path
+
+    dlc_output_dir = dj.config.get("custom", {}).get("dlc_output_dir")
+    if dlc_output_dir:
+        return Path(dlc_output_dir)
+    else:
+        return None
+
 
 # Activate "lab", "subject", "session" schema -------------
 
