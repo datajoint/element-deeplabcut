1969e5911b5aa6e56be908e1124cd95d4d91949d
Milagros Marin <milagros@datajoint.com>
2023-09-29 22:57:00 +0200

    Add `env` variables and prefix to dj database

---
+++ element_deeplabcut/__init__.py
@@ -0,0 +1,17 @@
+import os
+import datajoint as dj
+
+if "custom" not in dj.config:
+    dj.config["custom"] = {}
+
+# overwrite dj.config['custom'] values with environment variables if available
+
+dj.config["custom"]["database.prefix"] = os.getenv(
+    "DATABASE_PREFIX", dj.config["custom"].get("database.prefix", "")
+)
+
+dj.config["custom"]["dlc_root_data_dir"] = os.getenv(
+    "DLC_ROOT_DATA_DIR", dj.config["custom"].get("dlc_root_data_dir", "")
+)
+
+db_prefix = dj.config["custom"].get("database.prefix", "")
